6369624ba894dee17ae960f667f10426da474750
jenchieh <jcs090218@gmail.com>
2018-09-28 02:50:50 +0800

    Add setup scripts.

---
+++ scripts/setup.bat
@@ -0,0 +1,16 @@
+@echo off
+:: ========================================================================
+:: $File: setup.bat $
+:: $Date: 2018-09-27 17:04:31 $
+:: $Revision: $
+:: $Creator: Jen-Chieh Shen $
+:: $Notice: See LICENSE.txt for modification and distribution information
+::                   Copyright © 2018 by Shen, Jen-Chieh $
+:: ========================================================================
+
+
+:: Back to root directory.
+cd ..
+
+:: Install all dependecies.
+npm install express --save
---
+++ scripts/setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# ========================================================================
+# $File: setup.sh $
+# $Date: 2018-09-27 17:04:56 $
+# $Revision: $
+# $Creator: Jen-Chieh Shen $
+# $Notice: See LICENSE.txt for modification and distribution information
+#                   Copyright © 2018 by Shen, Jen-Chieh $
+# ========================================================================
+
+
+# Back to root directory.
+cd ..
+
+# Install all dependecies.
+npm install express --save
