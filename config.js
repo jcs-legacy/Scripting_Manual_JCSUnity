131b9d52227c7ec1268e784b2e154c24d4bf0f0e
jenchieh <jcs090218@gmail.com>
2018-09-28 02:51:41 +0800

    Add config.

---
+++ config.js
@@ -0,0 +1,21 @@
+/**
+ * $File: config.js $
+ * $Date: 2018-09-27 17:11:04 $
+ * $Revision: $
+ * $Creator: Jen-Chieh Shen $
+ * $Notice: See LICENSE.txt for modification and distribution information
+ *                   Copyright © 2018 by Shen, Jen-Chieh $
+ */
+
+"use strict";
+
+
+const HOST = "localhost";  // Host [Default: localhost]
+const PORT = 3000;         // Server Port [Default: 3000]
+
+const DEBUG = false;  // Debug mode?
+
+
+module.exports.DEBUG = DEBUG;
+module.exports.HOST = HOST;
+module.exports.PORT = PORT;
