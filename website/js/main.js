317a7257f4991515a7d0962a49c381ec29aa7a7a
jenchieh <jcs090218@gmail.com>
2018-09-28 02:53:39 +0800

    Add client's js entry.

---
+++ website/js/main.js
@@ -0,0 +1,26 @@
+/**
+ * $File: main.js $
+ * $Date: 2018-09-27 20:38:08 $
+ * $Revision: $
+ * $Creator: Jen-Chieh Shen $
+ * $Notice: See LICENSE.txt for modification and distribution information
+ *                   Copyright © 2018 by Shen, Jen-Chieh $
+ */
+
+"use strict";
+
+
+/**
+ * jQuery entry.
+ */
+(function ($) {
+
+}(this.jQuery));
+
+/**
+ * Program Entry
+ */
+function main() {
+
+}
+main();
