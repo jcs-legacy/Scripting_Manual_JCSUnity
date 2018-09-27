1f2e7b364f558ee0838435ef13f61f9e8b8888ae
jenchieh <jcs090218@gmail.com>
2018-09-28 02:50:35 +0800

    Add run scripts.

---
+++ scripts/run.bat
@@ -0,0 +1,16 @@
+@echo off
+:: ========================================================================
+:: $File: run.bat $
+:: $Date: 2018-09-27 16:02:55 $
+:: $Revision: $
+:: $Creator: Jen-Chieh Shen $
+:: $Notice: See LICENSE.txt for modification and distribution information
+::                   Copyright © 2018 by Shen, Jen-Chieh $
+:: ========================================================================
+
+
+:: Back to root directory.
+cd ..
+
+:: Run the server.
+node main.js
---
+++ scripts/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# ========================================================================
+# $File: run.sh $
+# $Date: 2018-09-27 16:03:20 $
+# $Revision: $
+# $Creator: Jen-Chieh Shen $
+# $Notice: See LICENSE.txt for modification and distribution information
+#                   Copyright © 2018 by Shen, Jen-Chieh $
+# ========================================================================
+
+
+# Back to root directory.
+cd ..
+
+# Run the server.
+node main.js
