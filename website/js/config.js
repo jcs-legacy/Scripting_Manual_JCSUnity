9d1bc9c805d803508bc539fd8fac0e2c36368039
jenchieh <jcs090218@gmail.com>
2018-10-19 01:22:47 +0800

    Add search engine config

--- website/js/config.js
+++ website/js/config.js
@@ -20,6 +20,8 @@ const copyright_text = "Copyright © 2018 JCSUnity Group. Built: 2018-09-28.";
 
 // Default content page.
 const intro_content = 'intro';
+// Search page.
+const search_content = 'search';
 
 // Content extension to load.
 //
@@ -28,3 +30,9 @@ const intro_content = 'intro';
 //
 // SEE : confid.js in the project root directory.
 const content_extension = '.html';
+
+// Search Input config
+const si_manual_placeholder = "Search manual...";
+const si_api_placeholder = "Search scripting...";
+
+const si_input_size = 17;  // character unit.
