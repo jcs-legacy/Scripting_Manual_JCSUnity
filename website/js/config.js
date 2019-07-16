f1cfeae13f1588639331826c4a8321d172ad513a
jenchieh <jcs090218@gmail.com>
2019-07-16 22:23:40 +0800

    Update config.

--- website/js/config.js
+++ website/js/config.js
@@ -23,7 +23,7 @@ const version_title_06 = "";
 const version_title_07 = "";
 
 const version_num_01 = "1.9.1";
-const version_num_02 = "2019.1.9f1";
+const version_num_02 = "2019.1.10f1";
 const version_num_03 = "";
 const version_num_04 = "";
 const version_num_05 = "";
