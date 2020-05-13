329ce09b26dee0b8d850af76978185dba974da15
jenchieh <jcs090218@gmail.com>
2020-05-13 19:38:40 +0800

    Add config flag to show table header.

--- website/js/config.js
+++ website/js/config.js
@@ -9,15 +9,16 @@
 
 "use strict";
 
-// Manual
+/* Manual */
 const manual_name = "JCSUnity";
 
-// Theme
+/* Theme */
 const header_color = '#222C37';
-const arrow_color = '#19E3B1';
-const th_color = '#A2F2DE';
+const arrow_color  = '#19E3B1';
+const th_show      = true;       // table header
+const th_color     = '#A2F2DE';  // table header color
 
-// Version Information
+/* Version */
 const version_title_01 = "JCSUnity Version: ";
 const version_title_02 = "Unity Version: ";
 const version_title_03 = "";
@@ -34,15 +35,13 @@ const version_num_05 = "";
 const version_num_06 = "";
 const version_num_07 = "";
 
-// Copyright text to show on the website.
+/* Copyright */
 const copyright_text = "Copyright © 2018 JCSUnity Group. Built: 2018-09-28.";
 
-// Default content page.
-const intro_content = 'intro';
-// Search page.
-const search_content = 'search';
+const intro_content  = 'intro';   // Default content page.
+const search_content = 'search';  // Search page.
 
-// Homepage Link
+/* Homepage Link */
 const homepage_url = "https://github.com/jcs090218/JCSUnity";  // The actual link to your homepage.
 const homepage_text = "JCSUnity.github";  // Text to show on the browser
 
@@ -54,8 +53,8 @@ const homepage_text = "JCSUnity.github";  // Text to show on the browser
 // SEE : config.js in the project root directory.
 const content_extension = '.md';
 
-// Search Input config
+/* Search Input Text */
 const si_manual_placeholder = "Search manual...";
-const si_api_placeholder = "Search scripting...";
+const si_api_placeholder    = "Search scripting...";
 
 const si_input_size = 17;  // character unit.
--- website/js/main.js
+++ website/js/main.js
@@ -654,6 +654,11 @@
 
     let th = $('th');
     th.css('background-color', th_color);
+
+    if (th_show == false) {
+      let thead = $('thead');
+      thead.css('display', 'none');
+    }
   }
 
   /**
