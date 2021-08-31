c5f5f40d97a2564f2e9ad070ba2ce8600fc860a6
JenChieh <jcs090218@gmail.com>
2021-08-31 23:22:46 +0800

    Update config

--- website/js/config.js
+++ website/js/config.js
@@ -10,7 +10,7 @@
 "use strict";
 
 /* Manual */
-const manual_name = "JCSUnity";
+const manual_name = 'JCSUnity';
 
 /* Theme */
 const header_color = '#222C37';
@@ -19,32 +19,32 @@ const th_show      = true;       // table header
 const th_color     = '#A2F2DE';  // table header color
 
 /* Version */
-const version_title_01 = "JCSUnity Version: ";
-const version_title_02 = "Unity Version: ";
-const version_title_03 = "";
-const version_title_04 = "";
-const version_title_05 = "";
-const version_title_06 = "";
-const version_title_07 = "";
+const version_title_01 = 'JCSUnity Version: ';
+const version_title_02 = 'Unity Version: ';
+const version_title_03 = '';
+const version_title_04 = '';
+const version_title_05 = '';
+const version_title_06 = '';
+const version_title_07 = '';
 
-const version_num_01 = "2.2.1";
-const version_num_02 = "2021.1.1f1";
-const version_num_03 = "";
-const version_num_04 = "";
-const version_num_05 = "";
-const version_num_06 = "";
-const version_num_07 = "";
+const version_num_01 = '2.2.1';
+const version_num_02 = '2021.1.1f1';
+const version_num_03 = '';
+const version_num_04 = '';
+const version_num_05 = '';
+const version_num_06 = '';
+const version_num_07 = '';
 
 /* Copyright */
-const copyright_text = "Copyright © 2018 JCSUnity Group. Built: 2018-09-28.";
+const copyright_text = 'Copyright © 2018 JCSUnity Group. Built: 2018-09-28.';
 
 /* Others */
 const intro_content  = 'intro';   // Default content page.
 const search_content = 'search';  // Search page.
 
 /* Homepage Link */
-const homepage_url  = "https://github.com/jcs090218/JCSUnity";  // Link to your homepage.
-const homepage_text = "JCSUnity.github";                        // Text represent your homepage.
+const homepage_url  = 'https://github.com/jcs090218/JCSUnity';  // Link to your homepage.
+const homepage_text = 'JCSUnity.github';                        // Text represent your homepage.
 
 
 // Content extension to load.
@@ -55,7 +55,7 @@ const homepage_text = "JCSUnity.github";                        // Text represen
 const content_extension = '.md';
 
 /* Search Input Text */
-const si_manual_placeholder = "Search manual...";
-const si_api_placeholder    = "Search scripting...";
+const si_manual_placeholder = 'Search manual...';
+const si_api_placeholder    = 'Search scripting...';
 
 const si_input_size = 17;  // character unit.
